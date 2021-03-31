609c9a33efbf1d3ab7050b436e892200663afd18
Neel Sheth <nsheth109@gmail.com>
2021-03-31 18:13:58 -0400

    added the function to addmanager

--- index.js
+++ index.js
@@ -12,3 +12,37 @@ const Manager = require('./lib/Manager')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 
+// Function to addManager
+function addManager() {
+    return inquirer.prompt([{
+            type: "input",
+            name: "name",
+            message: "What is the name of the manager?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the Managers ID"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the Managers email address?"
+        },
+        {
+            type: "input",
+            name: "number",
+            message: "What is the Managers office number"
+        },
+        {
+            type: "list",
+            name: "progress",
+            message: "What do you want to do next?",
+            choices: [
+                "Add an Engineer team member",
+                "Add an Intern team member",
+                "No more members to add"
+            ]
+        }
+    ])
+}
